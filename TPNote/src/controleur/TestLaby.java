bda555dd9d4181a73f9ba936273fbfbc77c92df1
Victor Quidet <victor.quidet@gmail.com>
2020-05-28 15:42:49 +0200

    LabyVue

--- TPNote/src/controleur/TestLaby.java
+++ TPNote/src/controleur/TestLaby.java
@@ -10,5 +10,49 @@ package controleur;
  * @author victo
  */
 public class TestLaby {
+    /**
+     * Constructeur qui instancie l’objet en attribut de la classe Labyrinthe avec le fichier fic de type File en paramètre.
+     *  Cette méthode propage l’exception FileFormatException en cas de problème de format du fichier.
+     *
+     * @param fic : fichier du labyrinthe
+     * @throws FileFormatException : problème de format de ficher
+     */
+    public TestLaby(File fic) throws FileFormatException { … }
+
+ /**
+     * Déplacement récursif en profondeur dans le labyrinthe à partir des positions ligne et colonne en paramètres.
+     *  A chaque case visitée (voir la méthode getCase de la classe Labyrinthe) dans cette ligne et colonne, pas encore visitée
+     * (voir la méthode getVisited implémentée dans la classe  CaseImplementee), on indique qu’on la visite (voir la méthode
+     * setVisited implémentée dans la classe  CaseImplementee) et on affiche  les coordonnées de la case visitée et le
+     * labyrinthe (voir les 2 méthodes surchargées affiche et affiche de LabyConsole).
+     * Cette méthode retourne un booléen indiquant l’arrêt de déplacement : true si la sortie est atteinte
+     * (voir les positions private dans la classe Labyrinthe), false sinon.
+     *
+     * @param ligne de la case
+     * @param colonne de la case
+     * @return d'un booléen d'arrêt du déplacement
+     */
+    public boolean deplacerDFS(int ligne, int colonne) { … }
+
+ /**
+     * Déplacement aléatoire dans le labyrinthe : cette méthode utilise la méthodes autoMove de Labyrinthe qui peut
+     * déclencher l’exception  ImpossibleMoveException à attraper.
+     *  A chaque case visitée, on applique les mêmes instructions que celles commentées dans la méthode deplacerDFS.
+     * Cette méthode retourne un booléen indiquant l’arrêt de déplacement : true si la sortie est atteinte
+     * (voir les positions private dans la classe Labyrinthe), false sinon.
+     *
+     * @return d'un booléen d'arrêt du déplacement 
+     */
+    public boolean deplacerAuto() { … }
+
+ /**
+     * Le main instancie l’objet du LabyConsole en attribut. Il saisit le nom du fichier du labyrinthe. Puis il instancie un
+     * objet de la classe  TestLaby avec en paramètre le fichier, ce qui peut déclencher l’exception  FileFormatException
+     * à attraper en affichant un message d’erreur sur un problème de format du fichier. Il affiche ensuite le labyrinthe avec
+     * la bonne méthode affiche de LabyConsole. Il affiche le menu avec la méthode menu de LabyConsole. Selon le choix
+     * retourné par le menu, soit il se déplace en profondeur avec deplacerDFS (à partir de la position de départ définie dans 
+     * la classe Labyrinthe), soit aléatoirement avec deplacerAuto, soit il quitte le programme.
+     */
+    public static void main(String[] args) { … }
     
 }
--- TPNote/src/vue/LabyConsole.java
+++ TPNote/src/vue/LabyConsole.java
@@ -5,10 +5,88 @@
  */
 package vue;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+import model.*;
+
 /**
  *
  * @author victo
  */
 public class LabyConsole {
+    
+    /**
+     * Affiche le menu composé de 3 choix : déplacement aléatoire, en profondeur DFS ou quitter le programme.
+     * L’utilisateur doit saisir un choix de type int et recommencer tant qu’aucun des 3 choix proposés n’est valide,
+     *  y compris si l’utilisateur saisit des caractères au lieu d’un nombre entier.
+     * Il faut donc pouvoir convertir la saisie, y compris avec des caractères, en entier.
+     * Dans le cas où l’utilisateur saisit des caractères autres que des chiffres, cette conversion génère
+     * l’exception  NumberFormatException qu’il faut attraper pour afficher un message d’erreur,
+     * tout en pouvant recommencer la saisie.
+     *
+     * Cette méthode retourne le choix saisi.
+     *
+     * @return choix
+     */
+    public int menu() {
+        
+        
+        
+         String choix1 = "";
+         int choix = 10;
+        boolean erreur; //Forum Developez.net pour l'idée du do while 
+         do{
+             System.out.println("1 Deplacer automatique en profondeur \n 2 Deplacer aleatoire \n 0 Quitter \n Entrer votre choix : \n");
+             erreur =false;
+             try {
+           
+        Scanner in = new Scanner(System.in);
+        choix1 = in.next(); 
+        choix = Integer.parseInt(choix1);
+        
+              }
+           catch( NumberFormatException |InputMismatchException e){ // Le multicatch expliqué par openclassroom
+         erreur = true;
+         System.out.print("Merci de ne rentrer que des valeures numériques, et égale à 0, 1 ou 2 \n");
+         }
+             if(choix != 0 && choix != 1 && choix != 2){
+                 System.out.print("Merci de ne rentrer que des valeures numériques, et égale à 0, 1 ou 2 \n");
+                 erreur =true;
+             }
+         }
+         while (erreur);
+         return choix;
+
+              
+    }
+
+    /**
+     * Affiche les coordonnées positionX et positionY protected de la Case c en paramètre
+     *
+     * @param c 
+     */
+    public void affiche(Case c) { 
+    
+    }
+
+    /**
+     * Affiche un labyrinthe en mode console en se servant des méthodes du Labyrinthe laby en paramètre :
+     * afficher toutes les cases soit de CaseMur avec le caractère ‘X’, soit de CaseTrou avec le caractère ’_’
+     * en se servant de instanceof pour les distinguer, soit le caractère ‘V’ si la case est visitée (voir  la méthode
+     *  getVisited définie dans l’interface Case et implémentée dans la classe CaseImplementee).
+     *
+     * @param laby
+     */
+    
+    
+
+    public void affiche(Labyrinthe laby) {
+    
+    }
+    
+    public static void main(String[] args){
+        LabyConsole lab = new LabyConsole();
+        lab.menu();
+    }
     
 }
